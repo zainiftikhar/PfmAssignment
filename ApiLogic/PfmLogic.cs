d6c122fc91ef761aba9906a940e51fd029bae702
iftikharzain@gmail.com <iftikharzain@gmail.com>
2023-10-22 12:26:57 +0200

    Added the model and the logic class to get the data from csv file

---
+++ ApiLogic/PfmLogic.cs
@@ -0,0 +1,20 @@
+﻿using CsvHelper;
+using PfmAssignment.Models;
+using System.Globalization;
+
+namespace PfmAssignment.ApiLogic
+{
+    public class PfmLogic
+    {
+        public List<PfmData> _pfmData;
+
+        public PfmLogic()
+        {
+            using (var reader = new StreamReader("Data/counts.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                _pfmData = new List<PfmData>();
+            }
+        }
+    }
+}
---
+++ Models/PfmData.cs
@@ -0,0 +1,10 @@
+﻿namespace PfmAssignment.Models
+{
+    public class PfmData
+    {
+        public int SensorId { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+        public int Count { get; set; }
+    }
+}
